93a84603a9610d246dae3a199e53b7816b710c85
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-08-01 22:28:28 +0530

    movie detail popup

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
 import { Movie } from "../types";
+import MovieDetailPopup from "./MovieDetailPopup";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="movie-card">
-      <img
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        alt={movie.title}
-        onError={(e) => (e.currentTarget.src = "/placeholder.jpg")}
-        width={250}
-        height={375}
-      />
-      <div className="movie-card-container">
-        <div className="movie-title-container">
-          <h2>{movie.title}</h2>
-          <span>{parseFloat(movie.vote_average).toFixed(1)}</span>
+    <>
+      <div className="movie-card" onClick={() => setOpen(true)}>
+        <img
+          src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+          alt={movie.title}
+          onError={(e) => (e.currentTarget.src = "/placeholder.jpg")}
+          width={250}
+          height={375}
+        />
+        <div className="movie-card-container">
+          <div className="movie-title-container">
+            <h2>{movie.title}</h2>
+            <span>{parseFloat(movie.vote_average).toFixed(1)}</span>
+          </div>
+          <p className="movie-overview">{movie.overview}</p>
         </div>
-        <p className="movie-overview">{movie.overview}</p>
       </div>
-    </div>
+      <MovieDetailPopup movie={movie} open={open} setOpen={setOpen} />
+    </>
   );
 };
 
---
+++ src/components/MovieDetailPopup.tsx
@@ -0,0 +1,61 @@
+import { createPortal } from "react-dom";
+import { Movie } from "../types";
+import { useEffect } from "react";
+
+type MovieDetailPopupProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  movie: Movie;
+};
+
+const MovieDetailPopup = ({ open, setOpen, movie }: MovieDetailPopupProps) => {
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("disable-scroll");
+    } else {
+      document.body.classList.remove("disable-scroll");
+    }
+    return () => {
+      document.body.classList.remove("disable-scroll");
+    };
+  }, [open]);
+
+  return createPortal(
+    open && (
+      <div
+        className="modal"
+        onClick={(event) => {
+          if (event.target === event.currentTarget) {
+            setOpen(false);
+          }
+        }}
+      >
+        <div className="modal-content">
+          <img
+            onClick={() => setOpen(false)}
+            className="close-modal"
+            src="/cross.png"
+            alt="close modal"
+          />
+          <img
+            className="backdrop-img"
+            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            alt={movie.title}
+            width={600}
+            height={330}
+          />
+          <div className="movie-card-container">
+            <div className="movie-title-container">
+              <h2>{movie.title}</h2>
+              <span>{parseFloat(movie.vote_average).toFixed(1)}</span>
+            </div>
+            <p className="modal-overview">{movie.overview}</p>
+          </div>
+        </div>
+      </div>
+    ),
+    document.body
+  );
+};
+
+export default MovieDetailPopup;
