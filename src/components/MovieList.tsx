f2457d7261bc969d869ed89ff9b64922003f6a65
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-08-01 17:02:59 +0530

    search done

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -9,14 +9,17 @@ const MovieList = () => {
     const [movies, setMovies] = useState<Movie[]>([])
     const [loading, setLoading] = useState(false)
     const [page, setPage] = useState(1)
+    const [search, setSearch] = useState("")
     const loadMoreRef = useRef(null)
+
+    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()))
 
     const getMovies = async () => {
         setLoading(true)
         try {
             const url = `${import.meta.env.VITE_API_BASE_URL}/upcoming?api_key=${import.meta.env.VITE_API_KEY}&page=${page}`
             const res = await axios.get(url)
-            setMovies(prevMovies => [...prevMovies, ...res.data.results]) 
+            setMovies(prevMovies => [...prevMovies, ...res.data.results])
         } catch (error) {
             console.log(error)
         } finally {
@@ -30,19 +33,27 @@ const MovieList = () => {
 
     useIntersectionObserver({
         target: loadMoreRef,
-        onIntersect: ()=> setPage(prev=> prev + 1),
+        onIntersect: () => setPage(prev => prev + 1),
         enabled: movies.length > 0,
     });
 
     return (
         <div className="movies-container">
-            {loading && <div>Loading....</div>}
-            <div className="movies-grid">
-                {movies && movies.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-                <div ref={loadMoreRef} className={""}>
-                    {loading ? <div>Loading...</div> : ""}
+            <div className="search-input">
+                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search for upcoming movies"/>
+                {
+                    search ? <img src="/cross.png" alt="close" onClick={()=>setSearch("")} /> : <img src="/search.png" alt="search" />
+                }
+            </div>
+            <div className="movies-list">
+                {loading && <div>Loading....</div>}
+                <div className="movies-grid">
+                    {filteredMovies && filteredMovies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                    <div ref={loadMoreRef} className={""}>
+                        {loading ? <div>Loading...</div> : ""}
+                    </div>
                 </div>
             </div>
         </div>
