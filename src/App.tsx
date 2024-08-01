6bc7c72f3b6d2347ade62f456afdf35d4a840d85
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-08-01 22:52:47 +0530

    add scroll to top on logo click

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import MovieList from './components/MovieList'
+import MovieList from "./components/MovieList";
 
 function App() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
-      <nav >
-        <div className='container'>
-          <span className='logo'>KMDB</span>
+      <nav>
+        <div className="container">
+          <span className="logo" onClick={scrollToTop}>
+            KMDB
+          </span>
         </div>
       </nav>
-      <main className='container'>
+      <main className="container">
         <MovieList />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
